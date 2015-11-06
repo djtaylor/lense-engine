a716bc1a82188ee587e5868a7cf6327e9c269b93
David Taylor <djtaylor13@gmail.com>
2015-11-06 10:43:49 -0500

    Added the new stats app files

---
+++ usr/lib/python2.7/dist-packages/lense/engine/api/app/stats/models.py
@@ -0,0 +1,19 @@
+# Django Libraries
+from django.db import models
+
+class DBStatsRequest(models.Model):
+    """
+    Main database model for storing API request stats.
+    """
+    path        = models.CharField(max_length=128)
+    method      = models.CharField(max_length=6)
+    client_ip   = models.CharField(max_length=15)
+    endpoint_ip = models.CharField(max_length=15)
+    retcode     = models.IntegerField()
+    req_size    = models.IntegerField()
+    rsp_size    = models.IntegerField()
+    created     = models.DateTimeField(auto_now_add=True)
+    
+    # Custom table metadata
+    class Meta:
+        db_table = 'stats_requests'
---
+++ usr/lib/python2.7/dist-packages/lense/engine/api/app/stats/utils.py
@@ -0,0 +1,21 @@
+from lense.common.utils import valid, invalid, set_response
+from lense.engine.api.app.stats.models import DBStatsRequest
+
+def log_request_stats(**params):
+    """
+    Helper method for logging request stats.
+    """
+    DBStatsRequest(**params).save()
+
+class RequestGet:
+    """
+    Retrieve API request statistics.
+    """
+    def __init__(self, parent):
+        self.api = parent
+        
+    def launch(self):
+        """
+        Worker method for retrieving API request statistics.
+        """
+        return valid(set_response(DBStatsRequest.objects.all().values(), '[]'))
