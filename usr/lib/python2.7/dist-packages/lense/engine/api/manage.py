cc67784cc88eb69b8bc231d4d911eabef8de52c4
David Taylor <djtaylor13@gmail.com>
2015-11-29 13:11:04 -0500

    Fixed broken os import

--- usr/lib/python2.7/dist-packages/lense/engine/api/manage.py
+++ usr/lib/python2.7/dist-packages/lense/engine/api/manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from sys import argv
-from os.environ import setdefault
+from os import environ
 
 # Lense API Django project management
 if __name__ == "__main__":
-    setdefault("DJANGO_SETTINGS_MODULE", "lense.engine.api.core.settings")
+    environ.setdefault("DJANGO_SETTINGS_MODULE", "lense.engine.api.core.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(argv)
