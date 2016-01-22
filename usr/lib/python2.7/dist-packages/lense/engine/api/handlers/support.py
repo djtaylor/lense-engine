629ca68f86d20865c4760d9cd69e5e61b7fa6fb0
David Taylor <djtaylor13@gmail.com>
2016-01-22 08:21:25 -0500

    Added support handler

---
+++ usr/lib/python2.7/dist-packages/lense/engine/api/handlers/support.py
@@ -0,0 +1,23 @@
+from lense.engine.api.handlers import RequestHandler
+
+class Support_Get(RequestHandler):
+    """
+    Retrieve a listing of API capabilities.
+    """
+    def launch(self):
+        """
+        Worker method to retrieve a listing of API handlers.
+        """
+        support  = {}
+        
+        # Get handlers
+        for handler in LENSE.OBJECTS.HANDLER.get():
+            support[handler.name] = {
+                'uuid': handler.uuid,
+                'path': handler.path,
+                'method': handler.method,
+                'desc': handler.desc
+            }
+        
+        # Return server capabilities
+        return self.ok(data=support)
