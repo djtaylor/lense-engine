0408da653203965536e21c991c92fffe2f2c06a2
David Taylor <djtaylor13@gmail.com>
2016-06-30 14:29:48 -0400

    Updated comment

--- usr/lib/python2.7/dist-packages/lense/engine/api/manage.py
+++ usr/lib/python2.7/dist-packages/lense/engine/api/manage.py
@@ -2,7 +2,7 @@
 from sys import argv
 from os import environ
 
-# Lense API Django project management
+# Lense project management
 if __name__ == "__main__":
     environ.setdefault("DJANGO_SETTINGS_MODULE", "lense.engine.api.core.settings")
     from django.core.management import execute_from_command_line
